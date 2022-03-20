4ef216a2bc3c2e313db65357c8e592afaabcf1c8
UrosSkrjanc <37333345+UrosSkrjanc@users.noreply.github.com>
2022-03-20 19:57:58 +0100

    Add files via upload

--- programming_assignment1/crawler/storage/storage.py
+++ programming_assignment1/crawler/storage/storage.py
@@ -1,52 +1,58 @@
 import threading
 import psycopg2 
 
-lock = threading.Lock()
-
-host="localhost"
-user="postgres"
-password="postgres"
-
 datatype=['DOC','DOCX','PDF','PPT','PPTX']
 
+#class Storage(object):
 class Storage(object):
 
-    def __init__(self):
-        self.conn = psycopg2.connect(host=host, user=user, password=password)
+    def __init__(self,configuration):
+        host = configuration.get('database_host')
+        user = configuration.get('database_user')
+        password = configuration.get('database_password')
+        database = configuration.get('database_name')
+
+        self.lock = threading.Lock()
+
+        self.conn = psycopg2.connect(database=database, host=host, user=user, password=password)
         self.conn.autocommit = True
 
+
     def add_site(self, domain, robots_content, sitemap_content):
-        if Storage.contains_site(self,domain)==0:
-            with lock:
+        if self.contains_site(domain)==0:
+            with self.lock:
                 cur = self.conn.cursor()
                 cur.execute("INSERT INTO crawldb.site (domain, robots_content, sitemap_content) VALUES (%s, %s, %s)", (domain, robots_content, sitemap_content))
+                cur.close()
             return True
         else:
             return False
 
 
     def update_site(self, domain, robots_content, sitemap_content):        
-        if Storage.contains_site(self, domain)!=0:
-            with lock:
+        if self.contains_site(domain)!=0:
+            with self.lock:
                 cur = self.conn.cursor()
                 cur.execute("UPDATE crawldb.site SET robots_content=%s, sitemap_content=%s WHERE domain=%s", (robots_content, sitemap_content,domain))
+                cur.close()
             return True
         else:
             return False
 
 
     def contains_site(self,domain):
-        with lock:
+        with self.lock:
             cur = self.conn.cursor()
             cur.execute("SELECT id FROM crawldb.site WHERE domain=%s", (domain,))
             dataFromBase=cur.fetchall()
+            cur.close()
         if len(dataFromBase)==0:
             return False
         return dataFromBase[0][0]
 
 
     def retrieve_site(self, domain):
-        with lock:
+        with self.lock:
             cur = self.conn.cursor()
             cur.execute("SELECT * FROM crawldb.site WHERE domain=%s", (domain,))
             dataFromBase=cur.fetchall()
@@ -58,10 +64,10 @@ class Storage(object):
 #---------------------------------------------------------------------------------
 
     def add_page(self, site, page_type_code, url, html_content, http_status_code, accessed_time):
-        if Storage.contains_page(self, site,url)==0:
-            pageSite=Storage.contains_site(self, site)
+        if self.contains_page(site,url)==0:
+            pageSite=self.contains_site(site)
             if pageSite!=0:
-                with lock:
+                with self.lock:
                     cur = self.conn.cursor()
                     cur.execute("INSERT INTO crawldb.page (site_id, page_type_code, url, html_content, http_status_code, accessed_time) VALUES (%s, %s, %s, %s, %s, %s)", (pageSite,page_type_code.upper(), url, html_content, http_status_code, accessed_time))
                     cur.close()
@@ -73,10 +79,10 @@ class Storage(object):
     
     
     def update_page(self, site, page_type_code, url, html_content, http_status_code, accessed_time):
-        if Storage.contains_page(self, site,url)!=0:
-            pageSite=Storage.contains_site(self, site)
+        if self.contains_page(site,url)!=0:
+            pageSite=self.contains_site(site)
             if pageSite!=0:
-                with lock:
+                with self.lock:
                     cur = self.conn.cursor()
                     cur.execute("UPDATE crawldb.page SET site_id=%s,page_type_code=%s, html_content=%s, http_status_code=%s, accessed_time=%s WHERE url=%s", (pageSite,page_type_code, html_content, http_status_code, accessed_time, url))
                     cur.close()
@@ -88,10 +94,10 @@ class Storage(object):
     
     
     def contains_page(self, site,url):
-        pageSite=Storage.contains_site(self, site)
+        pageSite=self.contains_site(site)
         if pageSite==False:
             pageSite=0
-        with lock:
+        with self.lock:
             cur = self.conn.cursor()
             cur.execute("SELECT id FROM crawldb.page WHERE url=%s AND site_id=%s", (url,pageSite))
             dataFromBase=cur.fetchall()
@@ -102,10 +108,10 @@ class Storage(object):
     
     
     def retrieve_page(self, site,url):
-        pageSite=Storage.contains_site(self, site)
+        pageSite=self.contains_site(site)
         if pageSite==False:
             pageSite=0
-        with lock:
+        with self.lock:
             cur = self.conn.cursor()
             cur.execute("SELECT crawldb.site.domain, crawldb.page.* FROM crawldb.page  LEFT JOIN crawldb.site ON crawldb.page.site_id=site.id WHERE url=%s AND site_id=%s", (url,pageSite))
             dataFromBase=cur.fetchall()
@@ -117,14 +123,14 @@ class Storage(object):
 
 #------------------------------------------------
 
-    def contains_redirection(self, site1,url1,site2,url2):
-        url1ID=Storage.contains_page(self, site1,url1)
-        url2ID=Storage.contains_page(self, site2,url2)
+    def contains_redirection(self,site1,url1,site2,url2):
+        url1ID=self.contains_page(site1,url1)
+        url2ID=self.contains_page(site2,url2)
         if url1ID==False:
             url1ID=0
         if url2ID==False:
             url2ID=0
-        with lock:
+        with self.lock:
             cur = self.conn.cursor()
             cur.execute("SELECT * FROM crawldb.link WHERE from_page=%s AND to_page=%s", (url1ID,url2ID))
             dataFromBase=cur.fetchall()
@@ -135,11 +141,11 @@ class Storage(object):
 
 
     def add_redirection(self, site1,url1,site2,url2):
-        if Storage.contains_redirection(self, site1,url1,site2,url2)==0:
-            url1ID=Storage.contains_page(self, site1,url1)
-            url2ID=Storage.contains_page(self, site2,url2)
+        if self.contains_redirection(site1,url1,site2,url2)==0:
+            url1ID=self.contains_page(site1,url1)
+            url2ID=self.contains_page(site2,url2)
             if url1ID!=0 and url2ID!=0:
-                with lock:
+                with self.lock:
                     cur = self.conn.cursor()
                     cur.execute("INSERT INTO crawldb.link (from_page, to_page) VALUES(%s,%s)", (url1ID,url2ID))
                     cur.close()
@@ -153,9 +159,9 @@ class Storage(object):
 #------------------------------------------------
 
     def add_image(self, site,url,filename,content_type,data,accessed_time):
-        pageID=Storage.contains_page(self, site,url)
+        pageID=self.contains_page(site,url)
         if pageID!=0:
-            with lock:
+            with self.lock:
                 cur = self.conn.cursor()
                 cur.execute("INSERT INTO crawldb.image (page_id,filename,content_type,data,accessed_time) VALUES(%s,%s,%s,%s,%s)", (pageID,filename,content_type,data,accessed_time))
                 cur.close()
@@ -164,9 +170,9 @@ class Storage(object):
             return False
 
     def contains_image(self, site,url,filename):
-        pageID=Storage.contains_page(self, site,url)
+        pageID=self.contains_page(site,url)
         if pageID!=0:
-            with lock:
+            with self.lock:
                 cur = self.conn.cursor()
                 cur.execute("SELECT * FROM crawldb.image WHERE page_id=%s AND filename=%s", (pageID,filename))
                 dataFromBase=cur.fetchall()
@@ -178,11 +184,11 @@ class Storage(object):
             return False
 
     def list_images(self, site,url):
-        pageID=Storage.contains_page(self, site,url)
+        pageID=self.contains_page(site,url)
         if pageID==False:
             pageID=0
         dataFromBase=[]
-        with lock:
+        with self.lock:
             cur = self.conn.cursor()
             cur.execute("SELECT * FROM crawldb.image WHERE page_id=%s", (pageID,))
             dataFromBase=cur.fetchall()
@@ -193,9 +199,9 @@ class Storage(object):
 # 
 
     def add_pagedata(self, site,url,data_type_code, data):
-        pageID=Storage.contains_page(self, site,url)
+        pageID=self.contains_page(site,url)
         if pageID!=0 and data_type_code.upper() in datatype:
-            with lock:
+            with self.lock:
                 cur = self.conn.cursor()
                 cur.execute("INSERT INTO crawldb.page_data (page_id, data_type_code,data) VALUES(%s,%s,%s)", (pageID,data_type_code.upper(),data))
                 cur.close()
@@ -203,10 +209,10 @@ class Storage(object):
         else:
             return False
 
-    def contains_pagedata(self, site,url,data_type_code,data):
-        pageID=Storage.contains_page(self, site,url)
+    def contains_pagedata(self,site,url,data_type_code,data):
+        pageID=self.contains_page(site,url)
         if pageID!=0:
-            with lock:
+            with self.lock:
                 cur = self.conn.cursor()
                 cur.execute("SELECT * FROM crawldb.page_data WHERE page_id=%s AND data=%s AND data_type_code=%s", (pageID,data,data_type_code.upper()))
                 dataFromBase=cur.fetchall()
@@ -218,10 +224,10 @@ class Storage(object):
             return False
 
     def list_pagedata(self, site,url):
-        pageID=Storage.contains_page(self, site,url)
+        pageID=self.contains_page(site,url)
         dataFromBase=[]
         if pageID!=0:
-            with lock:
+            with self.lock:
                 cur = self.conn.cursor()
                 cur.execute("SELECT * FROM crawldb.page_data WHERE page_id=%s", (pageID,))
                 dataFromBase=cur.fetchall()
