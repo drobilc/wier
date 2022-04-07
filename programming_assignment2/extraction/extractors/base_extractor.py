27d723e6998cc5b64f33400f1765aab4707453cc
Niki Bizjak <drobilc@gmail.com>
2022-04-07 13:39:28 +0200

    Add base extractor class

---
+++ programming_assignment2/extraction/extractors/base_extractor.py
@@ -0,0 +1,42 @@
+import argparse
+from os.path import join
+import glob
+
+class BaseExtractor(object):
+
+    def __init__(self):
+        pass
+
+    def parse_arguments(self, arguments):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('input_folder')
+        self.add_arguments(parser)
+        arguments, _ = parser.parse_known_args(arguments)
+        self.configuration = arguments
+    
+    def extract(self):
+        input_folder = self.configuration.input_folder
+
+        # Find all HTML files inside input folder
+        glob_pathname = join(input_folder, '*.html')
+        html_files = glob.glob(glob_pathname)
+
+        results = []
+
+        # Open each file and extract data
+        for filename in html_files:
+            with open(filename, 'r') as html_file:
+                try:
+                    file_content = html_file.read()
+                    result = self.extract_data(file_content)
+                    results.append(result)
+                except Exception as exception:
+                    print(exception)
+        
+        return results
+    
+    def add_arguments(self, parser):
+        pass
+    
+    def extract_data(self, content):
+        return {}
